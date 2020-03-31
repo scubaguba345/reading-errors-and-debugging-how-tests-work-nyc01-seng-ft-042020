bba8d1f4382f83f95cfff85f8ac03d85b56e341b
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-31 00:31:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,7 @@
-# Add your variables here
-first_number = ()
-expect (first_number).to be (Integer)
+first_number = 7
+second_number = 11
+
+sum = first_number + second_number
+difference = second_number - first_number
+product = first_number * second_number
+quotient = first_number / second_number
