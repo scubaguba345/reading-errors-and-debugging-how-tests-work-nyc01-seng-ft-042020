75b58fffc1a56846c9e04b5c191a151b7a1e4115
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-31 00:31:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,6 @@ first_number = 7
 second_number = 11
 
 sum = first_number + second_number
-difference = second_number - first_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
